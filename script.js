f19baefe2de82b636e63acc60dc288697c8b8892
adi-makes <adimakesprojects@gmail.com>
2025-07-24 13:59:37 +0530

    added functionality to buttons

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 const menuIcon = document.querySelector('#menu-icon');
 const navLinks = document.querySelector('.nav-links');
+const downloadCVButton = document.getElementById('downloadCVBtn');
+
+const cvFilePath = 'Resume230725.pdf';
+const suggestedFileName = 'Adith_R_Lal_CV.pdf';
 
 menuIcon.onclick = () => {
     navLinks.classList.toggle('active');
+}
+
+if(downloadCVButton){
+    downloadCVButton.addEventListener('click', function() {
+        const link = document.createElement('a');
+        link.href = cvFilePath;
+        link.download = suggestedFileName;
+
+        document.body.appendChild(link);
+
+        link.click();
+
+        document.body.removeChild(link);
+    });
 }
